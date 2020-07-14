c365cd83ba88ee4d3dac494e849f15efc9036fbf
Peter Lam <Peter.Lam@tufts.edu>
2020-07-14 16:21:56 -0400

    Updated server.js service for production

--- server.js
+++ server.js
@@ -13,5 +13,15 @@ mongoose.connect(db)
     .catch(err => console.log(err));
 
 const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 app.listen(port, () => console.log(`Server started on port ${port}`))
+
+
