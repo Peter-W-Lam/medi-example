7665a546cdb8f035d7b346de88bc5161532813a3
Peter Lam <Peter.Lam@tufts.edu>
2020-07-15 17:28:30 -0400

    Updated routing for serving build folder

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ router.use('/api/users', userRoutes)
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, './client/build/index.html'));
+	console.log("in router use")
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
